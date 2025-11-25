cf47834005f1721e47ce4dfb705234d945290b4e
ankitkumarpal731 <96754831+ankitkumarpal731@users.noreply.github.com>
2025-11-25 08:26:51 +0530

    config.py
    Updated sensitive configuration values for BOT_TOKEN, API_ID, API_HASH, and AUTH_USERS.

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216840269:AAFNzApyRlXuZ94T3UZgd-XqNun2mA40kJU")
-    API_ID = int(os.environ.get("API_ID", "7856100"))
-    API_HASH = os.environ.get("API_HASH", "dba409d7e8d44ab5bb689378deea1792")
-    AUTH_USERS = "1411895712"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8558068232:AAH5KdtBDzqBFjVJ0Tam7z9CgbUVOG8RXWo")
+    API_ID = int(os.environ.get("API_ID", "27351883"))
+    API_HASH = os.environ.get("API_HASH", "e571454385d31ca3035c0c9dd69e78fa")
+    AUTH_USERS = "8245649452"
+
 
